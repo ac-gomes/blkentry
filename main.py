4ed23e9e3844b9499fdec30f21d54c067c265cf2
ac-gomes <acgoms88@gmail.com>
2023-03-11 14:50:34 -0300

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+screen = Jinja2Templates(directory="templates")
+
+
+@app.get('/index/', response_class=HTMLResponse)
+def index(request: Request):
+    context = {'request': request}
+    return screen.TemplateResponse("index.html", context)
